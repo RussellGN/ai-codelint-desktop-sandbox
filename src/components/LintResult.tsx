69bbc5bd1505f2b674d37fc3408a621b7a8c4721
russellgn <dudurussell@gmail.com>
2026-04-22 14:22:38 +0200

    fix: handle empty lint results gracefully

--- src/components/LintResult.tsx
+++ src/components/LintResult.tsx
@@ -14,12 +14,14 @@ export function LintResult({
    error,
    closeDiagnostics,
 }: LintResultProps) {
-   const lintResultDisplay = lintResult.map((r, i) => (
-      <div key={i} className={`py-2 ${i === 0 ? "" : "border-t-2"}`}>
-         <mark className="bg-red-300 text-DARK p-0.5 font-bold text-sm">{`lines ${r.start_line}-${r.end_line}\n`}</mark>
-         {r.overview}
-      </div>
-   ));
+   const lintResultDisplay = lintResult.length
+      ? lintResult.map((r, i) => (
+           <div key={i} className={`py-2 ${i === 0 ? "" : "border-t-2"}`}>
+              <mark className="bg-red-300 text-DARK p-0.5 font-bold text-sm">{`lines ${r.start_line}-${r.end_line}\n`}</mark>
+              {r.overview}
+           </div>
+        ))
+      : null;
 
    return (
       <div className="bg-DARK text-red-300 flex flex-col min-h-0 overflow-hidden">
